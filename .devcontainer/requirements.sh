5e3e97124747efe6bdeebd014612bd4d956a49a6
Dario Curreri <48800335+dariocurr@users.noreply.github.com>
2021-11-09 23:44:36 +0100

    Update requirements.sh
    #4

--- .devcontainer/requirements.sh
+++ .devcontainer/requirements.sh
@@ -1,7 +1,7 @@
-while read package version; do \
-	if [ -z $version ]; then \
-	Rscript -e "devtools::install_version('"$package"')";
-else \
-	Rscript -e "devtools::install_version('"$package"', '"$version"')";
-fi; \
-	done < "/tmp/r-tmp/requirements.txt"
+while read package version; do
+	if [ -z $version ]; then
+		Rscript -e "devtools::install_version('"$package"')";
+	else
+		Rscript -e "devtools::install_version('"$package"', '"$version"')";
+	fi;
+done < "/tmp/r-tmp/requirements.txt"
