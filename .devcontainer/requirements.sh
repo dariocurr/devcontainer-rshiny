95f34919819a6a79ea91fb8ac104d8439def01d8
Dario Curreri <dariocurr@gmail.com>
2021-11-09 21:36:09 +0000

    add requirements.txt #4

---
+++ .devcontainer/requirements.sh
@@ -0,0 +1,7 @@
+while read package version; do \
+	if [ -z $version ]; then \
+	Rscript -e "devtools::install_version('"$package"')";
+else \
+	Rscript -e "devtools::install_version('"$package"', '"$version"')";
+fi; \
+	done < "/tmp/r-tmp/requirements.txt"
