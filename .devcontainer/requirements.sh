2fe37a991a51f261875dfd752da2fecb0e04aa5f
Dario Curreri <dariocurr@gmail.com>
2021-11-20 16:56:28 +0100

    quieting everything

--- .devcontainer/requirements.sh
+++ .devcontainer/requirements.sh
@@ -1,8 +1,8 @@
 # extract all packages specified in the requirements.txt file
 while read package version; do
 	if [ -z $version ]; then
-		Rscript -e "devtools::install_version('"$package"')";
+		Rscript -e "devtools::install_version('"$package"', quiet=TRUE)";
 	else
-		Rscript -e "devtools::install_version('"$package"', '"$version"')";
+		Rscript -e "devtools::install_version('"$package"', '"$version"', quiet=TRUE)";
 	fi;
 done < "/tmp/r-tmp/requirements.txt"
